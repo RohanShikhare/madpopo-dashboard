e935afca5e63d38a33c449d911ab00a8b860289d
Rohxn0802 <rohanshikhare08@gmail.com>
2024-03-29 12:51:38 +0530

    new files added

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("li.open").prev("li").addClass("before-open");
+  $("li.open").next("li").addClass("after-open");
+  $(".sideMenuTitle").click(function () {
+    $(".sideBarDiv").toggleClass("collapsed-sidebar");
+    if ($(".sideBarDiv").hasClass("collapsed-sidebar")) {
+        $('[data-bs-toggle="tooltip"]').tooltip(); 
+      } else {
+        $('[data-bs-toggle="tooltip"]').tooltip("dispose"); 
+      }
+  });
+ 
+});
