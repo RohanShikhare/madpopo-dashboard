f242a63ffbb37af576c722ecdacf0e39f5fa194d
Rohxn0802 <rohanshikhare08@gmail.com>
2024-04-01 12:34:19 +0530

    add-category-modal-complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,21 @@ $(document).ready(function () {
   $(".sideMenuTitle").click(function () {
     $(".sideBarDiv").toggleClass("collapsed-sidebar");
     if ($(".sideBarDiv").hasClass("collapsed-sidebar")) {
-        $('[data-bs-toggle="tooltip"]').tooltip(); 
-      } else {
-        $('[data-bs-toggle="tooltip"]').tooltip("dispose"); 
-      }
+      $('[data-bs-toggle="tooltip"]').tooltip();
+    } else {
+      $('[data-bs-toggle="tooltip"]').tooltip("dispose");
+    }
   });
   $(".datepicker").datepicker({
-    dateFormat: "MM yy"
+    dateFormat: "MM yy",
   });
- 
+  $("#nh-cat-input-add-wrap").on("click", function () {
+    $("#add-btn-modal").modal("show");
+  });
+  $("#ac-modal-closebtn").on("click", function () {
+    $("#add-btn-modal").modal("hide");
+  });
+  $("#bottom-close-btn").on("click", function () {
+    $("#add-btn-modal").modal("hide");
+  });
 });
